0ea95fa1fe9cf65c00b291307d6c17aec3d96296
Matias Hettich Castillo <matiashettich@outlook.com>
2024-02-28 15:14:49 -0300

    Added different angles for the ball to bounce when the player paddle is hitted depending of how far from the center the collision was

--- scripts/ball_controller.gd
+++ scripts/ball_controller.gd
@@ -10,20 +10,38 @@ func _ready():
 	reset_ball()
 
 func _physics_process(_delta):
-		
+	
 	var collision = move_and_collide(velocity * _delta)
 	if collision:
 		var collider = collision.get_collider()
 		if str(collider).contains("player"):
 			print("player hit!")
+			# Get the player node after collision
+			# Calculate the distance from the center of the paddle
+			var player = get_node("../player")
+			var difference = position.x - player.position.x
 			
-			# Change this later for different angles depending on wich part of
-			# Get collision with horizontal collider
-			if collision.get_normal() in [Vector2(0, 1), Vector2(0, -1)]:
-				velocity *= Vector2(1, -1)
-			# Get collision with vertical collider
-			else:
-				velocity *= Vector2(-1, 1)
+			# Determine the bounce direction
+			var direction = 1 if difference > 0 else -1
+			
+			# Set a bit of bounce angle in the center of the paddle
+			if difference >= -1 and difference <= 1:
+				velocity = Vector2(0.1, -1) * Vector2(direction, 1)
+				velocity.normalized()
+				velocity *= SPEED
+				
+			# Set some bounce angle in between the center and the sides
+			elif (difference >= -6 and difference < -1) or (difference > 1 and difference <= 6):
+				velocity = Vector2(0.75, -1) * Vector2(direction, 1)
+				velocity.normalized()
+				velocity *= SPEED
+				
+			# Set a lot of bounce angle on the sides of the paddle
+			elif difference < -6 or difference > 6:
+				velocity = Vector2(1.25, -1) * Vector2(direction, 1)
+				velocity.normalized()
+				velocity *= SPEED
+				
 		else:
 			# Handle brick collision
 			if str(collider).contains("brick"):
